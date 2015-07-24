3ab2622f80e162063d511da35572358a42022ba6
vernkennedy <vzidana14@wooster.edu>
2015-07-23 18:01:10 -0700

    2

--- javascripts/PHPProjects.js
+++ javascripts/PHPProjects.js
@@ -8,51 +8,41 @@ var PHPProjectSPortfolio =
       "owner": {       
         "avatar_url": "ProjectPortfolioTracker.jpg", 
       },           
-      "description": "https://github.com/redguide/nodejs",      
-      "code_url": "https://api.github.com/repos/redguide/nodejs",  
+      "description": "https://github.com/vernkennedy/Project-Portfolio-Tracker/blob/master/description.txt",      
+      "code_url": "https://github.com/vernkennedy/Project-Portfolio-Tracker",  
 	   "demo_url": "https://www.youtube.com/watch?v=lJM2oFMoDkM",
       "frontend_infor": "Html5,CSS3,JavaScript,jQuery, Google Charts/Google Visualization API",      
       "backend_infor": "Php5,SQL,MySQL"    
     },
-    {
-		"id":2,
-        "project_name": "Database Driven Google Map",
-      "owner": {       
-        "avatar_url": "DatabaseDrivenMaps.jpg", 
-      },           
-      "description": "https://github.com/redguide/nodejs",      
-      "code_url": "https://api.github.com/repos/redguide/nodejs", 
-	   "demo_url": "https://www.youtube.com/watch?v=lJM2oFMoDkM",
-      "frontend_infor": "Html5,CSS3,JavaScript,Google Maps API",      
-      "backend_infor": "Php5,SQL,MySQL"  
-    },
-  
 	
-	 {
-		"id":3,
+	{
+		"id":2,
        "project_name": "ScotBoard",
       "owner": {       
         "avatar_url": "ScotBoard.jpg", 
       },           
-      "description": "https://github.com/redguide/nodejs",      
-      "code_url": "https://www.youtube.com/watch?v=lJM2oFMoDkM",
+      "description": "https://github.com/vernkennedy/IS-ScotBoard/blob/master/description.txt",      
+      "code_url": "https://github.com/vernkennedy/IS-ScotBoard",
 	  "demo_url": "https://www.youtube.com/watch?v=lJM2oFMoDkM",
       "frontend_infor": "Html5,CSS3,JavaScript,Html5 Drag and Drop API",      
       "backend_infor": "Php5,SQL,MySQL"  
     },
-  
-    /*{
-		"id":4,
-      "project_name": "Drupal Module",
+	
+    {
+		"id":3,
+        "project_name": "Database Driven Google Map",
       "owner": {       
-        "avatar_url": "drupal.jpg", 
+        "avatar_url": "DatabaseDrivenMaps.jpg", 
       },           
-      "description": "https://github.com/redguide/nodejs",      
-      "code_url": "https://api.github.com/repos/redguide/nodejs", 
-	   "demo_url": "https://www.youtube.com/watch?v=lJM2oFMoDkM",
-      "frontend_infor": "Html5,CSS3",      
+      "description": "https://github.com/vernkennedy/Database-Google-Map/blob/master/description.txt",      
+      "code_url": "https://github.com/vernkennedy/Database-Google-Map", 
+	   "demo_url": "https://www.youtube.com/watch?v=QAgBDwnvH3I",
+      "frontend_infor": "Html5,CSS3,JavaScript,Google Maps API",      
       "backend_infor": "Php5,SQL,MySQL"  
-    }*/
+    }
+  
+	
+	 
   ]
 }
 
