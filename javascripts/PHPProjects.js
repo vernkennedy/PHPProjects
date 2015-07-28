ac75562b92cd3a2f654ce1a987fe6698e8bec7cb
vernkennedy <vzidana14@wooster.edu>
2015-07-28 07:43:46 -0700

    10

--- javascripts/PHPProjects.js
+++ javascripts/PHPProjects.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
-  $http.get("https://raw.githubusercontent.com/vernkennedy/PHPProjects/gh-pages/PHPProjects1.json")
+  $http.get("http://vernkennedy.github.io/PHPProjects/javascripts/PHPProjects.json")
   .success(function (response) {$scope.names = response.records;});
 });
